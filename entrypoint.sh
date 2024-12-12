34ebb50e5683ea97485f1f472eba8e3b0fe8da02
dknodes <luizamirlovaa@gmail.com>
2024-12-12 03:59:14 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo "WALLET_ADDRESS=$WALLET_ADDRESS"
-echo "CPU_CORES=$CPU_CORES"
-echo "RAM=$RAM"
-echo "DISK_SIZE=$DISK_SIZE"
-echo "DISK_SELECTION='$DISK_SELECTION'"
-
 node index.js
 
 tail -f /dev/null
